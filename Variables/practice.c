118533fb6cb4b8c19468e31a24d3d4aa60240a0d
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-02-07 16:37:19 +0000

    C practice

--- Variables/practice.c
+++ Variables/practice.c
@@ -1,25 +1,25 @@
 // Anthony Petersen, Template
 #include <stdio.h>
-char name[]: "Anthony";
-char number_10[]: "6";
-char number_1000[]: "856";
-char breakfast[]: "Nothing";
-char color[]: "Purple";
-char school_name[]: "UCAS";
-char year[]: "2025";
-char eye_color[]: "Brown";
-char age[]: "14";
-char favorite_subject[]: "CSP";
+char name[]= "Anthony";
+char number_10[]= "6";
+char number_1000[]= "856";
+char breakfast[]= "Nothing";
+char color[]= "Purple";
+char school_name[]= "UCAS";
+char year[]= "2025";
+char eye_color[]= "Brown";
+char age[]= "14";
+char favorite_subject[]= "CSP";
 int main(void){
-    printf("%s", name);
-    printf("%s", number_10);
-    printf("%s", number_1000);
-    printf("%s", breakfast);
-    printf("%s", color);
-    printf("%s", school_name);
-    printf("%s", year);
-    printf("%s", eye_color);
-    printf("%s", age);
-    printf("%s", favorite_subject);
+    printf("%s \n", name);
+    printf("%s \n", number_10);
+    printf("%s \n", number_1000);
+    printf("%s \n", breakfast);
+    printf("%s \n", color);
+    printf("%s \n", school_name);
+    printf("%s \n", year);
+    printf("%s \n", eye_color);
+    printf("%s \n", age);
+    printf("%s \n", favorite_subject);
     return 0;
 }
