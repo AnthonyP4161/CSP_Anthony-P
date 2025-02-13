d2c92e11a41636fcefd8a9d8d6a4591f145d8e73
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-13 11:23:42 -0700

    name decorator

--- Strings/name_decorator.py
+++ Strings/name_decorator.py
@@ -2,6 +2,6 @@
 name = (input("What is your name?").strip().capitalize())
 decoration1 = "(0_0)"
 decoration2 = "(0_0)"
-name1 = decoration1 + name
-name1 = name1 + decoration2
-print(name1)
+name = decoration1 + name
+name = name + decoration2
+print(name)
