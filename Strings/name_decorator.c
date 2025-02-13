3fffa038f7803aaaea8f4ca22ed27dc839958bfd
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-13 11:45:19 -0700

    idk

--- Strings/name_decorator.c
+++ Strings/name_decorator.c
@@ -1,7 +1,14 @@
 // Anthony Petersen, Template
 #include <stdio.h>
-
+#include <string.h>
+    char name[100];
 int main(void){
-    printf("Hello World");
-    return 0;
+    char decoration1[100] = "(O_O)";
+    char decoration2[100] = "(O_O)";
+    printf("Hello, what is your name: \n");
+     scanf("%s", name);
+    strcat(decoration1, name);
+    strcat(decoration1, decoration2);
+    printf("%s\n", decoration1);
+return 0;
 }
