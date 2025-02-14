642e93a956e0c04f38874717b95c0269910ef05c
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-14 11:21:40 -0700

    silly billy

---
+++ Boredom/idk.py
@@ -0,0 +1,6 @@
+print("Hi. This is quiz. On random stuff. Okay now.")
+low_taper_fade = input("What meme is absoultely MASSIVE? \n").strip().lower()
+if low_taper_fade == "low taper fade":
+    print("Wow, that's correct")
+else:
+        low_taper_fade = input("WRONG! What meme is absoultely MASSIVE? \n").strip().lower()
