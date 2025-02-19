9acedc27c3d5b6b5ebb5739d3a5d1db8dd36e3a6
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-19 11:34:24 -0700

    idk

--- Functions/hellow_world.c
+++ Functions/hellow_world.c
@@ -1,6 +1,13 @@
-// Anthony Petersen, Template
+// Anthony Petersen, Udate Hello World
 #include <stdio.h>
-
+void hello(char name[50]){
+    printf("Hello %s", name);
+}
 int main(void){
+    hello("Jack\n");
+    hello("John\n");
+    hello("Joe\n");
+    hello("Jeff\n");
+    hello("Jeffrey\n");
     return 0;
 }
