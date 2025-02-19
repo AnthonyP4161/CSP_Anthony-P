6b81013c20fee2e8ded5273457e9822cb8ff743a
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-19 11:10:31 -0700

    idk

--- Functions/notes.c
+++ Functions/notes.c
@@ -1,16 +1,24 @@
 // Anthony Petersen, C Functions Notes
 #include <stdio.h>
 int num;
-char name[50], place[50], verb[50];
-const char* word(char type[50]){
-    char choice[50];
-    print("Please give a %s", type);
-    scanf("%s", choice);
-    return choice;
+//char name[50], place[50], verb[50];
+//const char* word(char type[50]){
+   // char choice[50];
+    //print("Please give a %s", type);
+    //scanf("%s", choice);
+    //return choice;
+    
+//}
+void due(char assignment[50], char day[20]){
+    printf("The %s assignment is due %s", assignment, day);
+
 }
 int main(void){
-    printf("Please tell me a number: \n");
-    scanf("%d", num);
-    printf("%d", add(int numone+12));
+    //printf("Please tell me a number: \n");
+    //scanf("%d", num);
+    //printf("%d", add(int numone+12));
+    due("functions notes", "today\n");
+    due("Hello world update", "tommorow\n");
+    due("Financial calculator update", "Friday\n");
     return 0;
 }
