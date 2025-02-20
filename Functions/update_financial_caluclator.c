b58fcd89a23c0befe615a8fa8f6cd66aba48e6b4
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-20 11:49:19 -0700

    idk

---
+++ Functions/update_financial_caluclator.c
@@ -0,0 +1,24 @@
+// Anthony Petersen, Update Financial Calculator
+#include <stdio.h>
+#include <math.h>
+
+float income;
+float rent;
+float utilities;
+float groceries;
+float transportation;
+
+
+void inputs(char type[50], float *input){
+    printf("How much is your monthly %s in a dollar amount: \n", type);
+    scanf("%f", input);
+    return;
+}
+int main(void){
+    inputs("income", &income);
+    inputs("rent", &rent);
+    inputs("utilities", &utilities);
+    inputs("groceries", &groceries);
+    inputs("transportation", &transportation);
+    return 0;
+}
