a32743a20664e28c8217828579f67708a3318a4d
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-02-21 16:47:40 +0000

    idk

--- Functions/update_financial_caluclator.c
+++ Functions/update_financial_caluclator.c
@@ -8,14 +8,15 @@ float utilities;
 float groceries;
 float transportation;
 
-
 void inputs(char type[50], float *input){
     printf("How much is your monthly %s in a dollar amount: \n", type);
     scanf("%f", input);
     return;
 }
 void percentage(char type[50], float *number){
-    float type = number/income *100;
+    float percent = *number/income *100;
+    printf("Your %s is $%.2f which is %.1f%% of your income \n", type, *number, percent);
+    return;
 }
 int main(void){
     inputs("income", &income);
@@ -23,5 +24,14 @@ int main(void){
     inputs("utilities", &utilities);
     inputs("groceries", &groceries);
     inputs("transportation", &transportation);
+    percentage("rent", &rent);
+    percentage("utilities", &utilities);
+    percentage("groceries", &groceries);
+    percentage("transportation", &transportation);
+    float savings = income*.1;
+    float spending = income-rent-utilities-groceries-transportation-savings;
+    printf("Your savings is $%.2f which is 10%% of your income.\n", savings);  
+    printf("Your spending monet left over is $%.2f.\n", spending);  
+
     return 0;
 }
