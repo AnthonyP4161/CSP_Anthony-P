f58b212819d641dee5bd802f38a2a57fa32617f7
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-21 11:06:42 -0700

    idk

--- Expressions/financial_calculator.py
+++ Expressions/financial_calculator.py
@@ -8,7 +8,6 @@ income = float(input("What is your income in number amount: "))
 rent = float(input("How much does your rent cost: "))
 #have user input cost of utilities
 utilities = float(input("How much does your utilities such as power, water, etc. cost: "))
-utilities = "%.2f" % utilities
 #have user input cost of groceries
 groceries = float(input("How much do your groceries cost on average monthly: "))
 #have user input cost of transportation
@@ -36,9 +35,9 @@ transportation = "%.2f" % transportation
 #print the following
 
     #your rent is $ which is % of your income
-print("Your rent is", rent, "which is",percent_rent,"% of your income")
+print("Your rent is $", rent, "which is",percent_rent,"% of your income")
     #your utilities is $ which is % of your income
-print("Your utilties is", utilities, "which is",percent_utilities,"% of your income")
+print("Your utilties is $", utilities, "which is",percent_utilities,"% of your income")
     #your groceries is $ which is % of your income
 print("Your groceries is $", groceries, "which is",percent_groceries,"% of your income")
     #your transportation is $ which is % of your income
@@ -46,4 +45,4 @@ print("Your transportation is $", transportation, "which is",percent_transportat
     #your savings is $ which is % of your income
 print("Your savings is $", savings, "which is",percent_savings,"% of your income")
     #your spending is $ which is % of your income
-print("Your spending money leftover is", spending_money, "which is",percent_spending,"% of your income")
+print("Your spending money leftover is $", spending_money, "which is",percent_spending,"% of your income")
