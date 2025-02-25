923f2f299871b53347f518c478c577939d5061a8
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-02-25 16:52:13 +0000

    idk

--- Boredom/idk.py
+++ Boredom/idk.py
@@ -1,6 +1,8 @@
 print("Hi. This is quiz. On random stuff. Okay now.")
+score = float(0+1)
 low_taper_fade = input("What meme is absoultely MASSIVE? \n").strip().lower()
 if low_taper_fade == "low taper fade":
     print("Wow, that's correct")
+    score = float(score+1)
 else:
         low_taper_fade = input("WRONG! What meme is absoultely MASSIVE? \n").strip().lower()
