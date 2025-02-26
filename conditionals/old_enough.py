6b88a6ef9c2f1a634d93edc1fd75374c7dc3dff7
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-26 11:16:06 -0700

    idk

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -1,9 +1,11 @@
 age = float(input("How many years old are you: \n"))
-if(age <= 14):
-    print("You can go to school! \n")
+if(age >= 18):
+    print("You can vote! \n")
+elif(age >= 16):
+    print("You can get a drivers license! \n")
 elif(age == 15):
-    print("You can go to shcool and get your learners permit! \n")
-elif(age <= 17):
-    print("You can go to school, get a learners permit, and get a drivers license! \n")
+    print("You can get your learners permit! \n")
+elif(age >= 4):
+    print("You can go to school! \n")
 else:
-    print("You can go to school, get a learners permit, get a driver's license, and vote! \n")
+    print("You're a newborn, how are you even typing? \n")
