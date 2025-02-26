e4163202b81cf3b4069fadb7d3d3a380b3bb5165
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-26 11:48:41 -0700

    idk

--- Boredom/idk.py
+++ Boredom/idk.py
@@ -1,8 +1,23 @@
-print("Hi. This is quiz. On random stuff. Okay now.")
-score = float(0+1)
-low_taper_fade = input("What meme is absoultely MASSIVE? \n").strip().lower()
-if low_taper_fade == "low taper fade":
-    print("Wow, that's correct")
-    score = float(score+1)
+print("Hello, this is a quiz on random stuff. Be prepared, because you might get cooked! \n")
+score = 0
+question_1 =input("What meme is absolutely MASSIVE (P.S. Don't add too many letters because this meme may be DRAGGED out): \n").strip().lower()
+if question_1 == "low taper fade":
+    score = score+1
 else:
-        low_taper_fade = input("WRONG! What meme is absoultely MASSIVE? \n").strip().lower()
+    score = score
+question_2 = input("What game that came out in 2020 popularized the term sus: \n").strip().lower()
+if question_2 == "among us":
+    score = score+1
+else:
+    score = score
+question_3 = input("Who wears a mask with a smile, for hours at a time (P.S. They stare at a blank wall, well they hold back whats on their mind): \n").strip().lower()
+if question_3 == "dream":
+    score = score+1
+else:
+    score=score
+question_4 = input("In what way, does Logan Paul like his cheese (P.S. just enter the adjective)").strip().lower()
+if question_4 == "drippy":
+    score = score+1
+else:
+    score = score
+print("You got a score of", score, "out of 4")
