8d2e1c52e02d02316afc455d59c5b5239d5dd31f
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-02-28 11:49:12 -0700

    idk

---
+++ Boredom/random_experiment.py
@@ -0,0 +1,26 @@
+import random
+bot_number = random.randint(1,100)
+
+print("Welcome to sigma guesser! You will try to guess the same number as Master Sigma! \n")
+if input == 123456789:
+    print(bot_number)
+else:
+    bot_number == bot_number
+try:  
+    number = int(input("Please input a number between 1 and 100: \n"))  
+except ValueError:  
+    print("That's not a valid number! Please try again.")  
+if number == bot_number:
+    print("CORRECT! \n")
+else:
+    print("Try again!")
+    while number != bot_number:
+        try:  
+            number = int(input("Please input a number between 1 and 100: \n"))  
+        except ValueError:  
+            print("That's not a valid number! Please try again.")
+        if number == bot_number:  
+            print("CORRECT!")  
+            break  
+        else:  
+            print("Try again!")  
---
+++ Boredom/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+            print("That's not a valid number! Please try again.")
