69385ca06927b9f79d847a8979f4ad1a8f7bc60a
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-02-28 20:42:38 +0000

    idk

--- Boredom/brain_rot_quiz.py
+++ Boredom/brain_rot_quiz.py
@@ -1,49 +1,65 @@
-print("Hello, this is a quiz on random stuff. Be prepared, because you might get cooked!")
-print("Please note, all answers will consist of a single word answer, unless otherwise specified \n")
+print(" \nHello, this is a quiz on random stuff. Be prepared, because you might get cooked!")
+print(" \nPlease note, all answers will consist of a single word answer, unless otherwise specified \n")
 score = 0
 answers = ("Here are the answers you got wrong \n")
 
 
-question_1 =input("MULTI-WORD ANSWER \nWhat meme is absolutely MASSIVE: \n").strip().lower()
+question_1 =input("MULTI-WORD ANSWER \nQuestion 1 \nWhat meme is absolutely MASSIVE: \n").strip().lower()
 if question_1 == "low taper fade":
     score = score+1
 else:
     answers += "You entered " + question_1 + ", when the correct answer was low taper fade.\n"
 
 
-question_2 = input("MULTI-WORD ANSWER \nWhat game that came out in 2020 popularized the term sus: \n").strip().lower()
+question_2 = input("MULTI-WORD ANSWER \nQuestion 2 \nWhat game that came out in 2020 popularized the term sus: \n").strip().lower()
 if question_2 == "among us":
     score = score+1
 else:
     answers += "You entered " + question_2 + ", when the correct answer was among us.\n"
 
 
-question_3 = input("Who wears a mask with a smile, for hours at a time (P.S. They stare at a blank wall, well they hold back whats on their mind): \n").strip().lower()
+question_3 = input(" Question 3 \nWho wears a mask with a smile, for hours at a time (P.S. They stare at a blank wall, well they hold back whats on their mind): \n").strip().lower()
 if question_3 == "dream":
     score = score+1
 else:
     answers += "You entered " + question_3 + ", when the correct answer was dream.\n"
 
 
-question_4 = input("In what way, does Logan Paul like his cheese (P.S. this is a single word answer) \n").strip().lower()
+question_4 = input("Question 4 \nIn what way, does Logan Paul like his cheese (P.S. this is a single word answer) \n").strip().lower()
 if question_4 == "drippy":
     score = score+1
 else:
     answers += "You entered " + question_4 + ", when the correct answer was drippy.\n"
 
 
-question_5 = input("A word used to describe the leader of a group, seen as above a beta male \n").strip().lower()
+question_5 = input("Question 5 \nA word used to describe the leader of a group, often seen as better than a beta male \n").strip().lower()
 if question_5 == "alpha":
     score = score +1
 else:
     answers += "You entered " + question_5 + ", when the correct answer was alpha.\n"
 
 
+question_6 = input("MULTI-WORD ANSWER \nQuestion 6 \nWhat popular icon among the generation known as Gen Alpha, is a head coming out of a toilet \n").strip().lower()
+if question_6 == "skibidi toilet":
+    score = score+1
+else:
+    answers += "You entered " + question_6 + ", when the correct answer was skibidi toilet. \n"
+
+
+question_7 = input("Question 7 \nWhat was the name of the lunch food box created by Logan Paul, Mr. Beast, and KSI called \n").strip().lower()
+if question_7 == "lunchly":
+    score=score+1
+else:
+    answers += "You entered " + question_7 + ", when the correct answer was lunchly. \n"
+
 print("You got a score of", score, "out of 5 \n")
 
 
 review = input("If you would like to see the correct answer, type yes, otherwise type no \n")
 if review == "yes":
-    print((answers))
+    if score == 5:
+        print("You got everything right, so there's no wrong answers to show!")
+    else:
+        print((answers))
 else:
     print("Goodbye \n")
--- Boredom/random_experiment.py
+++ Boredom/random_experiment.py
@@ -2,12 +2,13 @@ import random
 bot_number = random.randint(1,100)
 
 print("Welcome to sigma guesser! You will try to guess the same number as Master Sigma! \n")
-if input == 123456789:
-    print(bot_number)
-else:
-    bot_number == bot_number
+
 try:  
-    number = int(input("Please input a number between 1 and 100: \n"))  
+    number = int(input("Please input a number between 1 and 100: \n"))
+    if number == 123456789:
+        print(bot_number)
+    else:
+        bot_number = bot_number
 except ValueError:  
     print("That's not a valid number! Please try again.")  
 if number == bot_number:
@@ -15,6 +16,7 @@ if number == bot_number:
 else:
     print("Try again!")
     while number != bot_number:
+        
         try:  
             number = int(input("Please input a number between 1 and 100: \n"))  
         except ValueError:  
