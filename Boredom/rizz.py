ed85c5d8f544223c931f0ae42f80e60ea2687f9a
AnthonyP4161 <Anthony.petersen@ucas-edu.net>
2025-03-17 11:36:19 -0600

    idk

---
+++ Boredom/rizz.py
@@ -0,0 +1,23 @@
+import random
+number = random.randint(1,10)
+if number == 1:
+    word = "sigma"
+elif number == 2:
+    word = "rizz"
+elif number == 3:
+    word = "skibidi"
+elif number == 4:
+    word = "ohio"
+elif number == 5:
+    word = "alpha"
+elif number == 6:
+    word = "baby gronk"
+elif number == 7:
+    word = "lunchly"
+elif number == 8:
+    word = "kai cenat"
+elif number == 9:
+    word = "property in egypt"
+else:
+    word = "skibidi toilet"
+print("This will generate a random brain rot word\n",word)
