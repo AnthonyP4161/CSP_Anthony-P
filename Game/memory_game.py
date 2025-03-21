5e5c9f5997123c55a462972cf125879556c85ee1
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-03-21 19:43:00 +0000

    idk

--- Game/memory_game.py
+++ Game/memory_game.py
@@ -37,12 +37,9 @@ frames = [
     [ x ] [ x ] [ x ] [ x ]
     """,
 ]  
-
-input("If you're ready type yes: \n")
-while input == "yes":
+print("This is a memory game where you will try to remember what hidden cards match, and which ones dont. When you believe you have gotten the correct answer, type the numbers starting at the top left, and moving right as if reading a book. ")
+while input("Type yes to start the game, or type your answer: ") == "yes" or input != (memory_1, memory_5, memory_2, memory_3, memory_2, memory_6, memory_1, memory_4, memory_3, memory_4, memory_6, memory_5) or input != "skibidi sigma":
     for frame in frames:
         os.system("cls" if os.name == "nt" else "clear")  # Clears screen for smooth animation
         print(frame)
-        time.sleep(.9)  # Adjust speed if needed
-    break
-
+        time.sleep(1)  # Adjust speed if needed
