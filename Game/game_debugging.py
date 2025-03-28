189a27eaa32c09fb821c42d1af634b72ad83c532
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-03-28 15:37:30 +0000

    idk

--- Game/game_debugging.py
+++ Game/game_debugging.py
@@ -307,7 +307,7 @@ def hangman():
 #Anthony Petersen
 def memory():
     clear_terminal()
-    delay = 0.035
+    delay = 0.0035
 
     ANinstructions = "This is a memory game that will flash the numbers and you have to repeat them.\n" \
                      "\n"\
@@ -435,12 +435,11 @@ def flashing_text3(stop_event):
 
 
 # sets the delay between each character printed
-delay = 0.04
 
 
 # Clear the terminal
 clear_terminal()
-
+delay = 0.0035
 
 # The Introduction - Jared
 message1 = "Welcome user...\n"\
