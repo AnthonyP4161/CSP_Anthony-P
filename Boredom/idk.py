c342bb64edf3fb0bc1acd50204e8dbf166cc5f1b
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-03-31 11:48:21 -0600

    idk

--- Boredom/idk.py
+++ Boredom/idk.py
@@ -3,32 +3,26 @@ import sys
 import threading
 import os
 import random
-player_block = """
-[   ()   ]
-[  -||-  ]
-[  _||_  ]"""
+map = "nothing"
+player_block =""" \
+[Y]"""
 
-empty = """
-[        ]
-[        ]
-[        ]"""
 
-rock = """
-[  ____ ]
-[ |    | ]
-[ |    | ]"""
+empty = """ \
+[ ]"""
 
+rock = """ \
+[R]"""
 
 def map_generation():
-    quadrant1 = random.randint(1,4)
-    quadrant2 = random.randint(1,4)
-    quadrant3 = random.randint(1,4)
-    quadrant4 = random.randint(1,4)
-    quadrant5 = random.randint(1,4)
-    quadrant6 = random.randint(1,4)
-    quadrant7 = random.randint(1,4)
-    quadrant8 = random.randint(1,4)
-    quadrant9 = random.randint(1,4)
+    quadrant1 = random.randint(1,3)
+    quadrant2 = random.randint(1,3)
+    quadrant3 = random.randint(1,3)
+    quadrant4 = random.randint(1,3)
+    quadrant5 = random.randint(1,3)
+    quadrant6 = random.randint(1,3)
+    quadrant7 = random.randint(1,3)
+    quadrant8 = random.randint(1,3)
     if quadrant1 == 3:
         quadrant1 = rock
     else:
@@ -41,6 +35,18 @@ def map_generation():
         quadrant3 = rock
     else:
         quadrant3 = empty
-    print(player_block, quadrant1, quadrant2,
-          quadrant3)
+    if quadrant4 == 3:
+        quadrant4 = rock
+    else:
+        quadrant4 = empty
+    if quadrant5 == 3:
+        quadrant5 = rock
+    else:
+        quadrant5 = empty
+    if quadrant6 == 3:
+        quadrant6 = rock
+    else:
+        quadrant6 = empty
+    map = print(player_block, quadrant1, quadrant2,"\n",
+          quadrant3, quadrant4, quadrant5)
 map_generation()
