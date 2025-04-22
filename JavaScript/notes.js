96158cb5fdf5e6e28d6b386dc53f2284a0c50d68
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-04-22 11:47:48 -0600

    JS Notes

---
+++ JavaScript/notes.js
@@ -0,0 +1,13 @@
+function hello(){
+    document.getElementById("title").style.color = "green"
+}
+function hi(){
+    document.getElementById("title").innerHTML = "Hi"
+}
+function restart(){
+    document.getElementById("title").innerHTML = "JS Notes"
+    document.getElementById("title").style.color = "black"
+}
+function hidden(){
+    document.getElementById("meme").style.display = "block"
+}
