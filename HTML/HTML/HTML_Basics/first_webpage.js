b6d1af7bf5c42d3081aa7c9503bcbeb9abb44801
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-04-25 11:49:05 -0600

    idk

---
+++ HTML/HTML/HTML_Basics/first_webpage.js
@@ -0,0 +1,14 @@
+function reveal(){
+    document.getElementById("invisible").style.display = "block" 
+    document.getElementById("hidden").innerHTML = "Hi"
+    
+}
+
+
+function hover(){
+    document.getElementById("img").src = "https://www.pngplay.com/wp-content/uploads/13/Pug-PNG-Images-HD.png"
+}
+
+function leave(){
+    document.getElementById("img").src = "https://images.vexels.com/media/users/3/300494/isolated/preview/e94c110b68f40df08116e06f9f5e22f2-cute-pug-dog-sitting-down.png"
+}
