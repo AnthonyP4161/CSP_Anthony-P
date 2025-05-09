54bc305484027498edb7fc6940d76e776e4092ce
AnthonyP4161 <anthony.petersen@ucas-edu.net>
2025-05-09 11:43:50 -0600

    idk

--- HTML/HTML/FINAL-Staycation/Staycation.js
+++ HTML/HTML/FINAL-Staycation/Staycation.js
@@ -1,13 +1,20 @@
 function reveal(){
-    var x = document.getElementById("hidden");
-    if (x.style.display === "none") {
-      x.style.display = "block";
+    if (document.getElementById("hidden").src === "https://www.nps.gov/cany/planyourvisit/images/_DSC4737.jpg?maxwidth=1300&maxheight=1300&autorotate=false") {
+      document.getElementById("hidden").src= "https://www.ulumresorts.com/app/uploads/2023/04/shutterstock_1801854883.jpg";
+      document.getElementById("credits").innerHTML = "Image by ulumresorts.com"
     } else {
-      x.style.display = "none";
+      document.getElementById("hidden").src = "https://www.nps.gov/cany/planyourvisit/images/_DSC4737.jpg?maxwidth=1300&maxheight=1300&autorotate=false";
+      document.getElementById("credits").innerHTML = "Image by Neal Herbert on nps.gov"
     }
 }
 
-function image_show(){
-  document.getElementById("image2").src= "https://www.ulumresorts.com/app/uploads/2023/04/shutterstock_1801854883.jpg"
 
+function hover(){
+  document.getElementById("image1").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWJSdHQWu2lMHKPqI2S2wLK3MvBiqRc6JO8Q&s"
+  document.getElementById("credits1").innerHTML = "Image by offthebeatenpath.com"
+}
+
+function leave(){
+  document.getElementById("image1").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Green_River_Overlook_Ekker_Butte.jpg/1920px-Green_River_Overlook_Ekker_Butte.jpg"
+  document.getElementById("credits1").innerHTML = "Image by au-ears on wikipedia.org"
 }
